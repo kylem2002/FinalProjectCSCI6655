3a80f3b2a650455d4f6c83a307dc28e091150f57
kylem2002 <146904543+kylem2002@users.noreply.github.com>
2023-12-06 01:35:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const fs = require('fs');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const app = express();
+const server = http.createServer((req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.setHeader('Access-Control-Allow-Methods', '*'); // Allowed methods
+    res.setHeader('Access-Control-Allow-Headers', '*'); // Allowed headers
 
-// Use CORS for all routes
-app.use(cors());
+    req.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin
+    req.setHeader('Access-Control-Allow-Methods', '*'); // Allowed methods
+    req.setHeader('Access-Control-Allow-Headers', '*'); // Allowed headers
 
-// Serve static files from 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+    // Handle OPTIONS method for preflight requests
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+    
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 
-// Fallback for handling non-existing routes
-app.use((req, res, next) => {
-    fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+    // Get the extension of the file
+    let extname = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+    switch (extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        // Add more cases for other file types if needed
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        // ... other file types
+    }
+
+    // Read file
+    fs.readFile(filePath, (err, content) => {
         if (err) {
-            res.writeHead(500);
-            res.end(`Server Error: ${err.code}`);
+            if (err.code == 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
         } else {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
             res.end(content, 'utf-8');
         }
     });
 });
 
 const PORT = process.env.PORT || 5500;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
