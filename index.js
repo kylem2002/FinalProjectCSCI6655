7140d34109736de015f95e08cd8679af175bc25a
kylem2002 <146904543+kylem2002@users.noreply.github.com>
2023-12-06 01:16:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,28 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE'); // Allowed methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
+const app = express();
 
-    // Handle OPTIONS method for preflight requests
-    if (req.method === 'OPTIONS') {
-        res.writeHead(204);
-        res.end();
-        return;
-    }
-    
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+// Use CORS for all routes
+app.use(cors());
 
-    // Get the extension of the file
-    let extname = path.extname(filePath);
+// Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // Initial content type
-    let contentType = 'text/html';
-
-    // Check ext and set content type
-    switch (extname) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        // Add more cases for other file types if needed
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        // ... other file types
-    }
-
-    // Read file
-    fs.readFile(filePath, (err, content) => {
+// Fallback for handling non-existing routes
+app.use((req, res, next) => {
+    fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
         if (err) {
-            if (err.code == 'ENOENT') {
-                // Page not found
-                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-                    res.writeHead(404, { 'Content-Type': 'text/html' });
-                    res.end(content, 'utf-8');
-                });
-            } else {
-                // Some server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
+            res.writeHead(500);
+            res.end(`Server Error: ${err.code}`);
         } else {
-            // Success
-            res.writeHead(200, { 'Content-Type': contentType });
+            res.writeHead(404, { 'Content-Type': 'text/html' });
             res.end(content, 'utf-8');
         }
     });
 });
 
 const PORT = process.env.PORT || 5500;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
